a09ed855cd08ed6e6c8bdb0c6a466302c1f3c936
RusyaevAleksandr <rusyaev1985@yandex.ru>
2024-08-15 20:34:59 +0500

    Исправил позиции кнопок. Добавил события нажатия на кнопки. Изменил отображение названия главного окна.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CalculatorWpfApp
 {
@@ -19,6 +10,86 @@ namespace CalculatorWpfApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DigitSevenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitEightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitNineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SumTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitFourButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitFiveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitSixButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SubtractButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitOneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitTwoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitThreeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DigitZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DivideButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
