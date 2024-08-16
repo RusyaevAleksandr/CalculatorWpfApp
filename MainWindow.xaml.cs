ad7dc469823816a56b7b511bf75a279a5c28f2de
Александр Русяев <rusyaev1985@mail.ru>
2024-08-16 15:43:36 +0500

    Добавил поля и тестовые функции для калькулятора. Доработать.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,84 +12,115 @@ namespace CalculatorWpfApp
             InitializeComponent();
         }
 
+        private int FirstDigit;
+
+        private int SecondDigit;
+
+        public int ResultNumber;
+
+        private string Sign;
+
+        private void AddingNumber(int digit)
+        {
+            if (Sign == null)
+            {
+                FirstDigit = digit;
+            }
+            else
+            {
+                SecondDigit = digit;
+            }
+        }
+
         private void DigitSevenButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(7);
         }
 
         private void DigitEightButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(8);
         }
 
         private void DigitNineButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(9);
         }
 
         private void SumTextButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sign = "+";
         }
 
         private void DigitFourButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(4);
         }
 
         private void DigitFiveButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(5);
         }
 
         private void DigitSixButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(6);
         }
 
         private void SubtractButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sign = "-";
         }
 
         private void DigitOneButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(1);
         }
 
         private void DigitTwoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(2);
         }
 
         private void DigitThreeButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(3);
         }
 
         private void MultiplyButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sign = "*";
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sign = "C";
         }
 
         private void DigitZeroButton_Click(object sender, RoutedEventArgs e)
         {
-
+            AddingNumber(0);
         }
 
-        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        private void ResultButton_Click(object sender, RoutedEventArgs e)
         {
-
+            switch (Sign)
+            {
+                case "+": ResultNumber = FirstDigit + SecondDigit; 
+                    break;
+                case "-": ResultNumber = FirstDigit - SecondDigit;
+                    break;
+                case "*": ResultNumber = FirstDigit * SecondDigit;
+                    break;
+                case "/": ResultNumber = FirstDigit / SecondDigit;
+                    break;
+                case "C": ResultNumber = 0; break;
+            }
         }
 
         private void DivideButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sign = "/";
         }
     }
 }
